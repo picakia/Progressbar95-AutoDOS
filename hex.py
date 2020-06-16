f361509e51e869a64a293776df0cab9a01fdb675
picakia <filipgil99@gmail.com>
2020-06-16 21:13:18 +0200

    Initial hex solver

---
+++ hex.py
@@ -0,0 +1,63 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import sys
+import subprocess
+import time
+import timeit
+
+print('Open game')
+subprocess.Popen(['am', 'start', '--activity-single-top', '-n', 'com.spookyhousestudios.progressbar95/com.ansca.corona.CoronaActivity'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
+#subprocess.Popen(['adb', 'devices'], stdout=subprocess.DEVNULL).wait()
+start_time = time.time()
+subprocess.Popen(['adb', 'shell', 'screencap', '-p', '/sdcard/dos.png']).wait()
+screen_time = time.time()
+game = Image.open('/sdcard/dos.png')
+width, height = game.size
+print(game.size)
+dos = game.crop(((width/27), (height/5.4), (width-(width/27)), (height/1.92)))
+#dos.save('/sdcard/vision.png', 'PNG')
+screen = pytesseract.image_to_string(dos, lang='eng', config='-c tessedit_do_invert=0').replace('\n', ' ')
+ocr_time = time.time()
+puzzle = screen.split(' ')
+for index, item in enumerate(puzzle):
+    if item == '':
+        puzzle.pop(index)
+    puzzle[index] = item.replace('i', '1').replace('l', '1')
+print('SCREEN:')
+print(screen)
+print('FORMATTED:')
+print(puzzle)
+solutionBest = []
+solutionMaybe = []
+solutionLow = []
+for number in puzzle:
+    if number != '':
+        if puzzle.count(number) > 4:
+            if number not in solutionBest:
+                solutionBest.append(number)
+        if puzzle.count(number) == 4:
+            if number not in solutionMaybe:
+                solutionMaybe.append(number)
+        if puzzle.count(number) == 3:
+            if number not in solutionLow:
+                solutionLow.append(number)
+solution_time = time.time()
+print('SOLUTION:')
+print('Best: ', solutionBest, ' Maybe: ', solutionMaybe, ' Low: ', solutionLow)
+if solutionBest:
+    subprocess.Popen(['adb', 'shell', 'input', 'text', solutionBest[0].upper()]).wait()
+    subprocess.Popen(['adb', 'shell', 'input', 'keyevent', '66']).wait()
+elif solutionMaybe:
+    subprocess.Popen(['adb', 'shell', 'input', 'text', solutionMaybe[0].upper()]).wait()
+    subprocess.Popen(['adb', 'shell', 'input', 'keyevent', '66']).wait()
+input_time = time.time()
+print('ELAPSED:')
+print('Screenshot time', (screen_time - start_time))
+print('OCR time', (ocr_time - screen_time))
+print('Solution time', (solution_time - ocr_time))
+print('Input time', (input_time - solution_time))
+print('Total', (time.time() - start_time))
+#subprocess.Popen(['am', 'start', '--activity-single-top', 'com.termux/com.termux.app.TermuxActivity'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).wait()
